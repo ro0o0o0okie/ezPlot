38954285dccf1c051c2579b3d77518cc8f85462a
RayN <nemo.ss433@163.com>
2018-01-31 00:07:53 +0800

    Refactor

--- yaxis_selector.py
+++ yaxis_selector.py
@@ -7,7 +7,7 @@
 ******************************
 """
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSlot
 
 import gui_base as gui
 
@@ -61,17 +61,53 @@ class UserDefinedStyle(object):
         #     line.set_markeredgecolor(self.marker_edgecolor)
     
 
+class StyleModifier(QtWidgets.QWidget):
+    
+    signal_modified = QtCore.pyqtSignal(str)
+    
+    def __init__(self, colName:str, parent=None, connectFunc=None):
+        super().__init__(parent)     
+        if connectFunc: 
+            self.signal_modified.connect(connectFunc)
+        
+        self.col_name = colName
+        self.editor_line_style = gui.ComboBox(   
+            label='LineStyle', 
+            textList=['','-','--','-.',':'], 
+            default='' )
+        self.editor_line_width = gui.Float(      
+            label='LineWidth(+/-)', 
+            low=-50, high=50, step=0.1, default=0, digits=2)
+        self.editor_marker = gui.ComboBox(   
+            label='Marker',
+            textList=['', 'o','>','<', 'v','^', 's', 'D', 'd', 'h','H'], 
+            default='')
+        self.editor_marker_size = gui.Float(      
+            label='MarkerSize(+/-)', 
+            low=-50, high=50, step=0.2, default=0, digits=2)
+        
+        self.editor_line_style.currentIndexChanged.connect(self.valueModified)
+        self.editor_line_width.valueChanged.connect(self.valueModified)
+        self.editor_marker.currentIndexChanged.connect(self.valueModified)
+        self.editor_marker_size.valueChanged.connect(self.valueModified)
+
+        self.setLayout(
+            gui.MakeFormLayout([
+                self.editor_line_style, 
+                self.editor_line_width, 
+                self.editor_marker, 
+                self.editor_marker_size ])
+        )
+    
+    def valueModified(self):
+        self.signal_modified.emit(self.col_name)
+    
+
 
 class DataFrameTree(QtWidgets.QTreeWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setHeaderLabel('DataFrameTree')
-        # self.df_nodes = dfNodes
-        # self.root_node = QtWidgets.QTreeWidgetItem(self)
-        # self.root_node.setText(0, dfname)
-        # self.root_node.setFlags(Qt.ItemIsEnabled) #| Qt.ItemIsEditable)
-        # self.addTopLevelItem(self.root_node)
-        
         self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection) # allow multiple selection
         self.setRootIsDecorated(True)
         self.setIndentation(20)
@@ -82,76 +118,74 @@ class DataFrameNode(QtWidgets.QTreeWidgetItem):
     """ A dataframe node, with multiple column names as sub-node,
         column sub-node's child is customs style widgets
     """
-    def __init__(self, dfName, columns, parent:DataFrameTree, styleChangedCallback=None):
+    def __init__(self, dfName, columns, parent:DataFrameTree,
+                 anyStyleChangedCallback=None, activeStyleChangedCallback=None):
         assert parent is not None
         super().__init__(parent)
+        self._cbk_any_changed = anyStyleChangedCallback # callback if any one of the columns style changed
+        self._cbk_active_changed = activeStyleChangedCallback # callback if the active/selected columns style changed
+        
         self.tree = parent
-        # self.root_node = QtWidgets.QTreeWidgetItem(self)
         self.setText(0, dfName)
         self.setFlags(Qt.ItemIsEnabled) #| Qt.ItemIsEditable)
         self.tree.addTopLevelItem(self)
         
-        # self.setRootIsDecorated(True)
-        # self.setIndentation(20)
-        # self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        
         self.column_names = columns
         self.column_user_styles = { c : UserDefinedStyle() for c in columns } # columnName => userStyleObj
+        self.column_items = {}
         
-        # styWgt = self.makeUserStyleWidget(styleChangedCallback)
+        
         for colname, usrsty in self.column_user_styles.items():
-            col = QtWidgets.QTreeWidgetItem(self)
+            col = self.column_items[colname] = QtWidgets.QTreeWidgetItem(self)
             col.setText(0, colname)
             col.setFlags(Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable)
-            # self.root_node.addTopLevelItem(col)
-            # self.column_items.append(col)
             
-            subItem = QtWidgets.QTreeWidgetItem(col)
-            subItem.setFlags(Qt.ItemIsEnabled)
-            self.tree.setItemWidget(subItem, 0, self.makeUserStyleWidget(styleChangedCallback))
-            
-            # for w in subWidgets:
-            #     # AsNamedWidget(w)
-            #     w = gui.WidgetWithLabel(w)
-            #     subItem = QtWidgets.QTreeWidgetItem(col)
-            #     self.setItemWidget(subItem, 0, w)
-            #     subItem.setFlags(Qt.ItemIsEnabled)
-        
-        # self.collapseAll()
-        # self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        # self.setRootIsDecorated(True)
-        # self.setIndentation(20)
+            styleModifier = QtWidgets.QTreeWidgetItem(col)
+            styleModifier.setFlags(Qt.ItemIsEnabled) # not selectable
+            # styleModifier.parent().isSelected()
+            modifier = StyleModifier(colName=colname, connectFunc=self.callback) # self.makeStyleModifierWidget(callback=styleChangedCallback)
+            self.tree.setItemWidget(styleModifier, 0, modifier)
+    
+    @pyqtSlot(str)
+    def callback(self, colname:str):
+        if self._cbk_any_changed:
+            self._cbk_any_changed(colname)
+        if self._cbk_active_changed:
+            if self.column_items[colname].isSelected():
+                self._cbk_active_changed(colname)
     
     
-    def makeUserStyleWidget(self, callback=None):
-        """ make the widget for user style control """
-        comboLs = gui.ComboBox(   
-            label='LineStyle', 
-            textList=['','-','--','-.',':'], 
-            default='' )
-        editorLw = gui.Float(      
-            label='LineWidth(+/-)', 
-            low=-50, high=50, step=0.1, default=0, digits=2)
-        comboMk = gui.ComboBox(   
-            label='Marker',
-            textList=['', 'o','>','<', 'v','^', 's', 'D', 'd', 'h','H'], 
-            default='')
-        editorMs = gui.Float(      
-            label='MarkerSize(+/-)', 
-            low=-50, high=50, step=0.2, default=0, digits=2)
-        
-        if callback:
-            comboLs.currentIndexChanged.connect(callback)
-            editorLw.valueChanged.connect(callback)
-            comboMk.currentIndexChanged.connect(callback)
-            editorMs.valueChanged.connect(callback)
-            
-        w = QtWidgets.QWidget()
-        w.setLayout(gui.MakeFormLayout([comboLs, editorLw, comboMk, editorMs]))
-        return w
+    # def makeStyleModifierWidget(self, callback=None):
+    #     """ make the widget for user style control """
+    #     comboLs = gui.ComboBox(   
+    #         label='LineStyle', 
+    #         textList=['','-','--','-.',':'], 
+    #         default='' )
+    #     editorLw = gui.Float(      
+    #         label='LineWidth(+/-)', 
+    #         low=-50, high=50, step=0.1, default=0, digits=2)
+    #     comboMk = gui.ComboBox(   
+    #         label='Marker',
+    #         textList=['', 'o','>','<', 'v','^', 's', 'D', 'd', 'h','H'], 
+    #         default='')
+    #     editorMs = gui.Float(      
+    #         label='MarkerSize(+/-)', 
+    #         low=-50, high=50, step=0.2, default=0, digits=2)
+    #     
+    #     if callback:
+    #         comboLs.currentIndexChanged.connect(callback)
+    #         editorLw.valueChanged.connect(callback)
+    #         comboMk.currentIndexChanged.connect(callback)
+    #         editorMs.valueChanged.connect(callback)
+    #         
+    #     w = QtWidgets.QWidget()
+    #     w.setLayout(gui.MakeFormLayout([comboLs, editorLw, comboMk, editorMs]))
+    #     return w
         
     
     def getUserStyles(self, col:str):
         return self.column_user_styles[col]
     
     
+
+
