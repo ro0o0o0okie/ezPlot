25f085669c020d46c6a450dacc01bf27978551a4
RayN <nemo.ss433@163.com>
2018-02-01 22:11:34 +0800

    ditto

--- yaxis_selector.py
+++ yaxis_selector.py
@@ -6,6 +6,9 @@
 * Created on 1/30/18
 ******************************
 """
+import os
+from collections import OrderedDict
+
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt, pyqtSlot
 
@@ -37,10 +40,8 @@ class UserDefinedStyle(object):
     
     def __bool__(self):
         """ return True if any of the styles is given """
-        return self.line_style or \
-               self.line_width_offset or \
-               self.marker or \
-               self.marker_size_offset 
+        return bool(
+            self.line_style or self.line_width_offset or self.marker or self.marker_size_offset )
     
     
     def apply(self, line):
@@ -63,129 +64,223 @@ class UserDefinedStyle(object):
 
 class StyleModifier(QtWidgets.QWidget):
     
-    signal_modified = QtCore.pyqtSignal(str)
+    signal_style_changed = QtCore.pyqtSignal()
     
-    def __init__(self, colName:str, parent=None, connectFunc=None):
-        super().__init__(parent)     
-        if connectFunc: 
-            self.signal_modified.connect(connectFunc)
+    def __init__(self, parent=None):
+        super().__init__(parent)
         
-        self.col_name = colName
+        self.style = UserDefinedStyle()
+        # if connectFunc: 
+        #     self.signal_modified.connect(connectFunc)
+        
+        # self.col_name = colName
         self.editor_line_style = gui.ComboBox(   
             label='LineStyle', 
             textList=['','-','--','-.',':'], 
-            default='' )
-        self.editor_line_width = gui.Float(      
+            default=self.style.line_style )
+        self.editor_line_width_offset = gui.Float(      
             label='LineWidth(+/-)', 
-            low=-50, high=50, step=0.1, default=0, digits=2)
+            low=-50, high=50, step=0.1, 
+            default=self.style.line_width_offset, digits=2)
         self.editor_marker = gui.ComboBox(   
             label='Marker',
             textList=['', 'o','>','<', 'v','^', 's', 'D', 'd', 'h','H'], 
-            default='')
-        self.editor_marker_size = gui.Float(      
+            default=self.style.marker)
+        self.editor_marker_size_offset = gui.Float(      
             label='MarkerSize(+/-)', 
-            low=-50, high=50, step=0.2, default=0, digits=2)
+            low=-50, high=50, step=0.2, 
+            default=self.style.marker_size_offset, digits=2)
         
         self.editor_line_style.currentIndexChanged.connect(self.valueModified)
-        self.editor_line_width.valueChanged.connect(self.valueModified)
+        self.editor_line_width_offset.valueChanged.connect(self.valueModified)
         self.editor_marker.currentIndexChanged.connect(self.valueModified)
-        self.editor_marker_size.valueChanged.connect(self.valueModified)
+        self.editor_marker_size_offset.valueChanged.connect(self.valueModified)
 
         self.setLayout(
             gui.MakeFormLayout([
                 self.editor_line_style, 
-                self.editor_line_width, 
+                self.editor_line_width_offset, 
                 self.editor_marker, 
-                self.editor_marker_size ])
+                self.editor_marker_size_offset ])
         )
     
+    @pyqtSlot()
     def valueModified(self):
-        self.signal_modified.emit(self.col_name)
+        self.style.line_style = self.editor_line_style.getValue()
+        self.style.line_width_offset = self.editor_line_width_offset.getValue()
+        self.style.marker = self.editor_marker.getValue()
+        self.style.marker_size_offset = self.editor_marker_size_offset.getValue()
+        self.signal_style_changed.emit()
     
+    def getStyle(self) -> UserDefinedStyle:
+        return self.style
+
 
 
 class DataFrameTree(QtWidgets.QTreeWidget):
-    def __init__(self, parent=None):
+    
+    
+    signal_column_renamed = QtCore.pyqtSignal(tuple) # (fn, oldName, newName) 
+    signal_active_style_changed = QtCore.pyqtSignal() 
+    
+    # signal_colnode_renamed = QtCore.pyqtSignal(tuple) # (oldName, newName)
+    # signal_colnode_selected_and_style_changed = QtCore.pyqtSignal()
+    
+    
+    def __init__(self, label='DataFrameTree', parent=None):
         super().__init__(parent)
-        self.setHeaderLabel('DataFrameTree')
+        # self.setHeaderLabel('DataFrameTree')
+        self.setHeaderHidden(True)
         self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection) # allow multiple selection
         self.setRootIsDecorated(True)
         self.setIndentation(20)
-
-
+        self.labelText = QtWidgets.QLabel(label+':')
+        
+        self.df_nodes = OrderedDict() # fn => DataFrameNode
+    
+    def addDataFrame(self, datafn, columns):
+        if datafn in self.df_nodes:
+            self.df_nodes[datafn].updateColumns(columnNames=columns)
+        else:
+            self.df_nodes[datafn] = \
+                DataFrameNode( parent=self, datafn=datafn, columnNames=columns )
+        return self.df_nodes[datafn]
+        
+        
+    def getSelectedColumns(self):
+        nodes = {} # fn => [selected column nodes]
+        for fn, dfnode in self.df_nodes.items():
+            cols = dfnode.getSelectedColumns()
+            if cols:
+                nodes[fn] = cols
+        return nodes
+    
+    # def getSelectedColumnNames(self, excludeName=None):
+    #     names = {} # fn => [selected column names]
+    #     for fn, cols in self.getSelectedColumns().items():
+    #         if excludeName:
+    #             names[fn] = [col.column_name for col in cols if cols.column_name!=excludeName]
+    #         else:
+    #             names[fn] = [col.column_name for col in cols]
+    #     return names
+    
+    
 
 class DataFrameNode(QtWidgets.QTreeWidgetItem):
     """ A dataframe node, with multiple column names as sub-node,
         column sub-node's child is customs style widgets
     """
-    def __init__(self, dfName, columns, parent:DataFrameTree,
-                 anyStyleChangedCallback=None, activeStyleChangedCallback=None):
+    # signal_column_renamed = QtCore.pyqtSignal(tuple) # (fn, oldName, newName) 
+    # signal_active_style_changed = QtCore.pyqtSignal() 
+    
+    def __init__(self, datafn, columnNames, parent:DataFrameTree,
+                 # activeStyleChangedCallback=None,  # f()
+                 # columnRenamedCallback=None  # f()
+                 ):
         assert parent is not None
         super().__init__(parent)
-        self._cbk_any_changed = anyStyleChangedCallback # callback if any one of the columns style changed
-        self._cbk_active_changed = activeStyleChangedCallback # callback if the active/selected columns style changed
-        
+        # self._cbk_any_style_changed = anyStyleChangedCallback # callback if any one of the columns style changed
+        # self._cbk_active_style_changed = activeStyleChangedCallback # callback if the active/selected columns style changed
+        self._hash = hash(datafn) # constant
+        self.datafile = datafn
+        self.dfname = os.path.basename(datafn) # can be renamed by user
+        self.setText(0, self.dfname)
         self.tree = parent
-        self.setText(0, dfName)
-        self.setFlags(Qt.ItemIsEnabled) #| Qt.ItemIsEditable)
         self.tree.addTopLevelItem(self)
+        self.setFlags(Qt.ItemIsEnabled) #| Qt.ItemIsEditable)
         
-        self.column_names = columns
-        self.column_user_styles = { c : UserDefinedStyle() for c in columns } # columnName => userStyleObj
-        self.column_items = {}
+        # self.column_names = columns
+        # self.column_user_styles = { c : UserDefinedStyle() for c in columns } # columnName => userStyleObj
+        self.column_nodes = [ DataColumnNode(colname=cn, parent=self) for cn in columnNames ] 
+        # for col in self.column_nodes:
+        #     print(col.signal_renamed, type(col.signal_renamed))
+        #     col.signal_renamed.connect(self.onColumnRenamed)
+        #     col.signal_selected_and_style_changed.connect(self.onActiveStyleChanged)
         
+        # for colname, usrsty in self.column_user_styles.items():
+        #     col = self.column_items[colname] = QtWidgets.QTreeWidgetItem(self)
+        #     col.setText(0, colname)
+        #     col.setFlags(Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable)
+        #     # col.
+        #     styleModifier = QtWidgets.QTreeWidgetItem(col)
+        #     styleModifier.setFlags(Qt.ItemIsEnabled) # not selectable
+        #     # styleModifier.parent().isSelected()
+        #     modifier = StyleModifier(colName=colname, connectFunc=self.onStyleChanged) # self.makeStyleModifierWidget(callback=styleChangedCallback)
+        #     self.tree.setItemWidget(styleModifier, 0, modifier)
+    
+    # @pyqtSlot(tuple) # (oldName, newName)
+    # def onColumnRenamed(self, names:tuple):
+    #     self.signal_column_renamed.emit((self.datafile, *names))
+    # 
+    # @pyqtSlot()
+    # def onActiveStyleChanged(self):
+    #     self.signal_active_style_changed.emit()
         
-        for colname, usrsty in self.column_user_styles.items():
-            col = self.column_items[colname] = QtWidgets.QTreeWidgetItem(self)
-            col.setText(0, colname)
-            col.setFlags(Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable)
+    # def onStyleChanged(self, colname:str):
+    #     # if self._cbk_any_style_changed:
+    #     #     self._cbk_any_style_changed(colname)
+    #     if self._cbk_active_style_changed:
+    #         if self.column_items[colname].isSelected():
+    #             self._cbk_active_style_changed(colname)
+    
+    def __hash__(self):
+        return self._hash
+    
+    def updateColumns(self, columnNames):
+        for c in self.column_nodes:
+            self.removeChild(c)
+        self.column_nodes.clear()
+        self.column_nodes = [ DataColumnNode(colname=cn, parent=self) for cn in columnNames ] 
+        
+    
+    def getSelectedColumns(self):
+        return [col for col in self.column_nodes if col.isSelected()]
+        
+    # def getSelectedColumnNames(self):
+    #     return [col.column_name for col in self.column_nodes if col.isSelected()] 
+    
+    
+    # def getUserStyles(self, col:str):
+    #     return self.column_user_styles[col]
+    
+    
+
+class DataColumnNode(QtWidgets.QTreeWidgetItem):
+    """A column name node, and its style modifier """
+    # signal_renamed = QtCore.pyqtSignal(tuple) # (oldName, newName)
+    # signal_selected_and_style_changed = QtCore.pyqtSignal()
+    
+    def __init__(self, colname, parent:DataFrameNode):
+        assert parent is not None
+        super().__init__(parent)
+        self.dfnode = parent
+        self.tree = parent.tree
+        self.column_name = colname
+        # self.style = UserDefinedStyle()
+        
+        self.setText(0, colname)
+        self.setFlags(Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable)
+        
+        self.modifier_modifier = StyleModifier()
+        self.modifier_modifier.signal_style_changed.connect(self.onStyleChanged)
+        item = QtWidgets.QTreeWidgetItem(self)
+        item.setFlags(Qt.ItemIsEnabled) # not selectable
+        self.tree.setItemWidget(item, 0, self.modifier_modifier)
+    
+    
+    def onStyleChanged(self):
+        if self.isSelected():
+            self.tree.signal_active_style_changed.emit()
+    
+    def setData(self, column, role, newName):
+        """override parent text edit function to send both old and new names"""
+        # if newName!=oldName:
+        super().setData(column, role, newName)
+        oldName = self.column_name
+        if role == Qt.EditRole and newName!=oldName:
+            self.column_name = newName
+            self.tree.signal_column_renamed.emit((self.dfnode.datafile, oldName, newName))
             
-            styleModifier = QtWidgets.QTreeWidgetItem(col)
-            styleModifier.setFlags(Qt.ItemIsEnabled) # not selectable
-            # styleModifier.parent().isSelected()
-            modifier = StyleModifier(colName=colname, connectFunc=self.callback) # self.makeStyleModifierWidget(callback=styleChangedCallback)
-            self.tree.setItemWidget(styleModifier, 0, modifier)
     
-    @pyqtSlot(str)
-    def callback(self, colname:str):
-        if self._cbk_any_changed:
-            self._cbk_any_changed(colname)
-        if self._cbk_active_changed:
-            if self.column_items[colname].isSelected():
-                self._cbk_active_changed(colname)
-    
-    
-    # def makeStyleModifierWidget(self, callback=None):
-    #     """ make the widget for user style control """
-    #     comboLs = gui.ComboBox(   
-    #         label='LineStyle', 
-    #         textList=['','-','--','-.',':'], 
-    #         default='' )
-    #     editorLw = gui.Float(      
-    #         label='LineWidth(+/-)', 
-    #         low=-50, high=50, step=0.1, default=0, digits=2)
-    #     comboMk = gui.ComboBox(   
-    #         label='Marker',
-    #         textList=['', 'o','>','<', 'v','^', 's', 'D', 'd', 'h','H'], 
-    #         default='')
-    #     editorMs = gui.Float(      
-    #         label='MarkerSize(+/-)', 
-    #         low=-50, high=50, step=0.2, default=0, digits=2)
-    #     
-    #     if callback:
-    #         comboLs.currentIndexChanged.connect(callback)
-    #         editorLw.valueChanged.connect(callback)
-    #         comboMk.currentIndexChanged.connect(callback)
-    #         editorMs.valueChanged.connect(callback)
-    #         
-    #     w = QtWidgets.QWidget()
-    #     w.setLayout(gui.MakeFormLayout([comboLs, editorLw, comboMk, editorMs]))
-    #     return w
-        
-    
-    def getUserStyles(self, col:str):
-        return self.column_user_styles[col]
-    
-    
-
-
+    def getStyle(self):
+        return self.modifier_modifier.getStyle()   
